80e6980d39e9109040b04bb7fe835ffe7aabe325
Anoop Kumar Sharma <anoop-sharma@users.noreply.github.com>
2021-02-13 21:57:00 +0530

    Version 3

---
+++ Assets/Script/GamePlay.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamePlay : MonoBehaviour {
+
+	public GameObject Green, RedBlue, White, Back;
+	int count;
+	//Vector3 spawnpos;
+	float spawntime = 2.0f;
+	
+	// Use this for initialization
+	void Start () {
+		Random.seed = 20;
+		//spawnpos = new Vector3 (0, 0, 100);
+		count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (count < 1) {
+			spawnTerrain (Random.Range (1, 10));
+			count++;
+		} else {
+			spawntime -= Time.deltaTime;
+			if(spawntime < 0)
+			{
+				spawntime = 2.0f;
+				count = 0;
+			}
+		}
+		/*if (count < 1) {
+			spawnTerrain (Random.Range (1, 10));
+			Vector3 temp = Randomvec();
+			spawnpos = new Vector3 (temp.x,temp.y, 100);
+			count++;
+		} else {
+			spawntime -= Time.deltaTime;
+			if(spawntime < 0)
+			{
+				spawntime = 2.0f;
+				count = 0;
+			}
+		}*/
+	}
+	
+	void spawnTerrain(int num)
+	{
+		Instantiate (Back, new Vector3(0,0,100), Quaternion.Euler (0, 0, 0));
+		if (num >= 1 && num < 3) {
+						/*Vector3 temp = new Vector3(0,4.98f,100);
+						Instantiate (White, temp, Quaternion.Euler(0,0,0));*/
+						Vector3 temp = new Vector3(0,0,100);
+						temp.y = Random.Range(-1.5f,1.5f);
+						temp.x = -Mathf.Sqrt (2.25f - (temp.y * temp.y));
+						Instantiate (White, temp, Quaternion.Euler(0,0,0));
+				} else if (num >= 3 && num < 5) {
+						/*Vector3 temp = new Vector3(0,-4.98f,100);
+						Instantiate (White, temp, Quaternion.Euler(0,0,0));*/
+						Vector3 temp = new Vector3(0,0,100);
+						temp.y = Random.Range(-1.5f,1.5f);
+						temp.x = Mathf.Sqrt (2.25f - (temp.y * temp.y));
+						Instantiate (White, temp, Quaternion.Euler(0,0,0));
+				} else if (num >= 5) {
+						Vector3 temp = new Vector3(0,0,100);
+						temp.y = Random.Range(-4.98f,4.98f);
+						temp.x = Mathf.Sqrt (25.0f - (temp.y * temp.y));
+						Instantiate (Green, temp, Quaternion.Euler(0,0,0));
+						temp.y = Random.Range(-4.98f,4.98f);
+						temp.x = -Mathf.Sqrt (25.0f - (temp.y * temp.y));
+						Instantiate (RedBlue, temp, Quaternion.Euler(0,0,0));
+				}
+
+		//Instantiate (Back, new Vector3(0,0,100), Quaternion.Euler (0, 0, 0));
+		/*if (num >= 1 && num < 4) {
+			Instantiate(Green,spawnpos,Quaternion.Euler(0,0,0));
+		} else if (num >= 4 && num < 7) {
+			Instantiate(RedBlue,spawnpos,Quaternion.Euler(0,0,0));
+		} else if (num >= 7) {
+			Instantiate(White,spawnpos,Quaternion.Euler(0,0,0));
+		}*/
+	}
+
+	/*Vector3 Randomvec()
+	{
+		Vector3 temp = new Vector3 (0, 0, 0);
+
+		do {
+						temp.x = Random.Range (-8.0f, 8.0f);
+						temp.y = Random.Range (-8.0f, 8.0f);
+				} while(((temp.x*temp.x)+(temp.y*temp.y)) == 64);
+
+		return temp;
+	}*/
+}
---
+++ Assets/Script/RingMovement.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RingMovement : MonoBehaviour
+{
+
+		bool destroyed = false;
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				Vector3 temp = transform.position;
+
+				if (temp.z <= 0.1f && temp.z >= - 0.1f) {
+						if (name == "R(Clone)" && GameObject.Find ("Player_2").transform.position.z == 0) {
+								Vector3 compare = GameObject.Find ("Player_2").transform.position;
+								float x_max = transform.position.x + renderer.bounds.size.x * 0.5f;
+								float x_min = transform.position.x - renderer.bounds.size.x * 0.5f;
+								float y_max = transform.position.y + renderer.bounds.size.y * 0.5f;
+								float y_min = transform.position.y - renderer.bounds.size.y * 0.5f;
+								if (compare.x <= x_max && compare.x >= x_min && compare.y <= y_max && compare.y >= y_min) {
+										PlayerMove.score++;
+										DestroyImmediate (gameObject);
+										destroyed = true;
+								}
+								/*float check = Mathf.Atan (compare.y / compare.x) * Mathf.Rad2Deg;
+								float max;
+								if (transform.rotation.eulerAngles.z >= 285) {
+										max = transform.rotation.eulerAngles.z - 360.0f;
+								} else {
+										max = transform.rotation.eulerAngles.z + 5.0f;
+								}
+								float min = max - 20.0f;
+
+								if (check >= min && check <= max) {
+										PlayerMove.score++;
+								}*/
+						} else if (name == "G(Clone)" && GameObject.Find ("Player_1").transform.position.z == 0) {
+								Vector3 compare = GameObject.Find ("Player_1").transform.position;
+								float x_max = transform.position.x + renderer.bounds.size.x * 0.5f;
+								float x_min = transform.position.x - renderer.bounds.size.x * 0.5f;
+								float y_max = transform.position.y + renderer.bounds.size.y * 0.5f;
+								float y_min = transform.position.y - renderer.bounds.size.y * 0.5f;
+								if (compare.x <= x_max && compare.x >= x_min && compare.y <= y_max && compare.y >= y_min) {
+										PlayerMove.score++;
+										DestroyImmediate (gameObject);
+										destroyed = true;
+								}
+								/*float check = Mathf.Atan (compare.y / compare.x) * Mathf.Rad2Deg + 180.0f;
+								float max = transform.rotation.eulerAngles.z + 5.0f;
+								float min = max - 20.0f;
+				
+								if (check >= min && check <= max) {
+										PlayerMove.score++;
+								}*/
+						} else if (name == "W(Clone)" && GameObject.Find ("Player").transform.position.z == 0) {
+								Vector3 compare = GameObject.Find ("Player").transform.position;
+								float x_max = transform.position.x + renderer.bounds.size.x * 0.5f;
+								float x_min = transform.position.x - renderer.bounds.size.x * 0.5f;
+								float y_max = transform.position.y + renderer.bounds.size.y * 0.5f;
+								float y_min = transform.position.y - renderer.bounds.size.y * 0.5f;
+								if (compare.x <= x_max && compare.x >= x_min && compare.y <= y_max && compare.y >= y_min) {
+										PlayerMove.score += 2;
+										destroyed = true;
+										DestroyImmediate (gameObject);
+								}
+								/*if ((transform.rotation.eulerAngles.z == 282.5f && GameObject.Find ("Player").transform.position.z == 0 && GameObject.Find ("Player").transform.position.y == 4.98f) ||
+										(transform.rotation.eulerAngles.z == 102.5f && GameObject.Find ("Player").transform.position.z == 0 && GameObject.Find ("Player").transform.position.y == -4.98f)) {
+										PlayerMove.score++;
+								}*/
+						}
+				}
+				if (destroyed == false) {
+						temp.z -= 0.3f;
+						transform.position = temp;
+
+						if (transform.position.z <= -10) {
+								DestroyImmediate (gameObject);
+						}
+				}
+		}
+}
